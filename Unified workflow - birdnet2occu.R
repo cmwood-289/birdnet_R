2aef9f5c053702b02d2a463971ad3bff9a4f7f4f
Connor Wood <cmw289@cornell.edu>
2026-06-11 10:45:09 -0400

    updated for pak::pak load

--- Unified workflow - birdnet2occu.R
+++ Unified workflow - birdnet2occu.R
@@ -1,13 +1,28 @@
 # Analyze Audio----
 
-install.packages("birdnetR") # developer: Felix Guenther
+pak::pak("birdnet-team/birdnetR")
+# error: 'could not find tools necessary to compile a package'
+# updated R (now 4.5.1), installed Rtools 45, then installation worked.
+
+#install.packages("birdnetR") # developer: Felix Guenther
 library(birdnetR) 
 
-install_birdnet() # prompted by "No version of birdnet found"
-reticulate::install_python(list = TRUE) # prompted by "Python release version '3.11' not found"
-
 # Initialize a BirdNET model
-model <- birdnet_model_tflite() # not working yet
+model <- load_birdnet() # how much version control do we have here?
+
+
+getwd()
+# Specify input audio
+# not currently working, waiting for Felix's input
+audio_path <- system.file("./test_data",
+                          "163134SoCalOwls02_032K_G25-C3642-U1_S0062_20250424_180000-0700.flac", 
+                          package = "birdnetR")
+
+file.path("./test_data/163134SoCalOwls02_032K_G25-C3642-U1_S0062_20250424_180000-0700.flac")
+
+
+# Analyze audio
+output <- as.data.frame(predict(model, audio_path))
 
 
 # Process outputs ----
