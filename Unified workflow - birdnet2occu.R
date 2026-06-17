f753e9b15c03ac216a727b64c74ae596cbcebe40
Connor Wood <cmw289@cornell.edu>
2026-06-17 08:49:26 -0400

    workflow complete through 'birdnetTools'

--- Unified workflow - birdnet2occu.R
+++ Unified workflow - birdnet2occu.R
@@ -1,7 +1,7 @@
 # A fully R-based workflow for BirdNET and occupancy modeling #
 
 
-# Analyze Audio----
+# Analyze Audio with 'birdnetR' ----
 
 pak::pak("birdnet-team/birdnetR") # developer: Felix Guenther
 # error: 'could not find tools necessary to compile a package'
@@ -44,21 +44,26 @@ for (folder in audio_folders) {
 
 ## Full-scale analysis ----
 
-# Specify the top-level audio folder
-all_folders <- list.dirs('S:/2023_USDA-USFS_CA_163134/163134_SoCalOwls_Dep02_FLAC',
-                           recursive = T)
+# Specify the top-level audio folder (used again with 'birdnetTools')
+top_audio_folder <- 'S:/2023_USDA-USFS_CA_163134/163134_SoCalOwls_Dep02_FLAC'
 
-# remove upper-level folders to retain only audio-containing folders
+# identify audio-containing folders
+all_folders <- list.dirs(top_audio_folder,
+                         recursive = T)
 audio_folders <- all_folders[!sapply(all_folders, function(d) {
   any(startsWith(all_folders[all_folders != d], paste0(d, "/")))
 })]
 
-length(audio_folders)
+# specify output location (folder must be manually created first)
+birdnet_output_location <- "./test_data/2025am_outputs" 
 
-# analyze audio
-birdnet_output_location <- "./test_data/2025am_outputs" # specify output location (folder must be manually created first)
 
-# note: 32kHz files run at 290-310xRT, a 50% penalty relative to 48kHz
+# analyze audio!
+
+# (not used yet) subset_by_time = "yes" # if 'yes', check hard-coded timestamps
+
+# note 1: 32kHz files run at 290-310xRT, a 50% penalty relative to 48kHz
+# note 2: need to manually check the time-stamp positions are hard-coded into the subsetting part
 
 for(folder in audio_folders) {
   files <- list.files(folder, pattern = "\\.flac$", full.names = TRUE)
@@ -80,17 +85,20 @@ for(folder in audio_folders) {
   print(paste(which(audio_folders==folder),"of", length(audio_folders), "input folders", sep=" "))
 }
 
+folder
+# processed 1:folder of audio_folders
+
 # 
 
 #
 
 
-# Process outputs ----
+# Process outputs with 'birdnetTools' ----
 
+## Continuation of 'Initial testing' ----
 pak::pak("birdnet-team/birdnetTools") # developer: Sunny Tseng
 library(birdnetTools)
 
-## Continuation of 'Initial testing' ----
 
 # combine input folder-level output files
 outputs_combined <- birdnet_combine(birdnet_output_location) # 'output_location' defined above in the 'analyze audio' step
@@ -118,15 +126,26 @@ MOQU <- birdnet_filter(outputs_combined,
 
 ## Continuation of 'Full-sale analysis'----
 
+remotes::install_github("birdnet-team/birdnetTools", 
+                        ref = "occupancy-modelling-functions")
+# skipped updates
+
+library(tidyverse)
+library(birdnetTools)
+
+
+
 # combine input folder-level output files
-outputs_combined <- birdnet_combine(birdnet_output_location) # 'output_location' defined above in the 'analyze audio' step
+full_outputs_combined <- birdnet_combine(birdnet_output_location) 
+# 'output_location' defined above in the 'analyze audio' step
+# note: can be slow when you have many thousands of output files for tens of thousands of hours of audio
 
 # minor cleaning for compatibility with downstream commands
-names(outputs_combined)[1] <- "filepath"
-outputs_combined$'Common Name' = sub(".*_", "", outputs_combined$species_name)
+names(full_outputs_combined)[1] <- "filepath"
+full_outputs_combined$'Common Name' = sub(".*_", "", full_outputs_combined$species_name)
 
 # filter to one species and score
-MOQU <- birdnet_filter(outputs_combined,
+MOQU <- birdnet_filter(full_outputs_combined,
                        species = "Mountain Quail",
                        threshold = 0.382855133,  # pr(tp)>= 0.9 per 'Thresholds_2024_20260601.csv'
                        #year = 2025,
@@ -134,7 +153,7 @@ MOQU <- birdnet_filter(outputs_combined,
                        #max_date = "2023-06-30",
                        #hour = c(0:23) 
                        )
-HEWA <- birdnet_filter(outputs_combined,
+HEWA <- birdnet_filter(full_outputs_combined,
                        species = "Hermit Warbler",
                        threshold = 0.565058755, # pr(tp)>= 0.9 per 'Thresholds_2024_20260601.csv'
                        #year = 2025,
@@ -142,13 +161,58 @@ HEWA <- birdnet_filter(outputs_combined,
                        #max_date = "2023-06-30",
                        #hour = c(0:23) 
                        )
+OSFL <- birdnet_filter(full_outputs_combined,
+                       species = "Olive-sided Flycatcher",
+                       threshold = 0.29307429, # pr(tp)>= 0.9 per 'Thresholds_2024_20260601.csv'
+                       #year = 2025,
+                       #min_date = "2023-04-01",
+                       #max_date = "2023-06-30",
+                       #hour = c(0:23)
+                       )
 
-# plenty of quails in these files, no Hermit Warblers
+# Make some heatmaps for fun
+birdnet_heatmap(MOQU)
+birdnet_heatmap(HEWA)
+birdnet_heatmap(OSFL)
 
+
+# Generate survey effort
+effort <- birdnet_get_effort(top_audio_folder, i = -3) 
+# note: numbers are slightly off because I filtered files by filename in the birdnet loop, not a problem here though
+
+# Build the encounter histories
+history_MOQU <- birdnet_detection_history(data = MOQU,
+                                          effort_data = effort,
+                                          survey_interval = "7 days")
+history_HEWA <- birdnet_detection_history(data = HEWA,
+                                          effort_data = effort,
+                                          survey_interval = "7 days")
+history_OSFL <- birdnet_detection_history(data = OSFL,
+                                          effort_data = effort,
+                                          survey_interval = "7 days")
+
+# history_output$detection_history  # The binary site-by-occasion matrix
+# history_output$detection_summary  # The long-format diagnostic dataframe
 #
 
 # Fit occupancy models ----
 
 library(unmarked) # developers: Fiske and Chandler (2011), Ken Kellner (2023)
 library(spOccupancy) # developer: Jeff Doser
+
+dim(effort)
+dim(history_OSFL$detection_history)
+dim(history_HEWA$detection_history)
+dim(history_OSFL$detection_history)
+
+umf_MOQU <- unmarkedFrameOccu(y = history_MOQU,
+                              siteCovs = Null,
+                              obsCovs = list(effort = effort_MOQU, # 'effort_MOQU' not yet created
+                                             time = matrix(c(rep(scale(seq(1,dim(history_MOQU$detection_history)[2], by=1)), 
+                                                                 dim(history_MOQU$detection_history)[1])),
+                                                           nrow = dim(history_MOQU$detection_history)[1], 
+                                                           ncol = dim(history_MOQU$detection_history)[2],
+                                                           byrow = T)) )
+
+                                             
 
